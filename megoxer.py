a9878aa149684d1cede91bcf213a926e9e0388c6
MEGOXERHUB9353 <githubmegoxer@gmail.com>
2025-01-30 08:17:16 +0530

    Update megoxer.py

--- megoxer.py
+++ megoxer.py
@@ -7,7 +7,7 @@ import threading
 from telebot import types
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7404351144:AAHBMvt2Ak3CuqtNfNEKq2R2-6WMEDMLHng')
+bot = telebot.TeleBot('6855294912:AAHT0EMYvVBM4ezim0qI-NcFdzsL-6BZ6eU')
 
 # Admin user IDs
 admin_id = {"7469108296"}
